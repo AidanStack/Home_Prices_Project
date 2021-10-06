4b45fb5cfaca0b55d46e5f2b662c6e8021807852
marvelje <jeffrey.w.marvel@gmail.com>
2021-10-06 00:01:04 -0400

    Add model permutations, function for checking correlation

--- Notebooks/Jeff/util.py
+++ Notebooks/Jeff/util.py
@@ -12,19 +12,26 @@ def run_model(df_train, df_test, cols, log_price):
     X_train = df_train.loc[:,cols]
     X_test = df_test.loc[:,cols]
     
-    formula = 'price ~ ' + ' + '.join(cols)
+    if log_price == True:
+        formula = 'price_log ~ ' + ' + '.join(cols)
+    else:
+        formula = 'price ~ ' + ' + '.join(cols)
     
     model = ols(formula=formula, data=df_train).fit()
     
-    train_preds = model.predict(X_train)
-    test_preds = model.predict(X_test)
-    
+    if log_price == True:
+        train_preds = np.exp(model.predict(X_train))
+        test_preds = np.exp(model.predict(X_test))
+    else:
+        train_preds = model.predict(X_train)
+        test_preds = model.predict(X_test)
+        
     return model, train_preds, test_preds
 
 
 def evaluate_model(df_train, df_test, cols, log_price):
 
-    model, train_preds, test_preds = run_model(df_train, df_test, cols)
+    model, train_preds, test_preds = run_model(df_train, df_test, cols, log_price)
     
     y_train = df_train['price']
     y_test = df_test['price']
@@ -56,3 +63,27 @@ def evaluate_model(df_train, df_test, cols, log_price):
     print('\n')
     print("Residual qq plot")
     sm.qqplot(train_residuals, line = 'r');
+
+# Filter for only multicollinearity above a certain threshold
+def high_corr(df, threshold):
+    mult_corr = df.corr().abs().stack().reset_index().sort_values(0, ascending=False)
+
+    # zip the variable name columns (Which were only named level_0 and level_1 by default) in a new column named "pairs"
+    mult_corr['pairs'] = list(zip(mult_corr.level_0, mult_corr.level_1))
+
+    # set index to pairs
+    mult_corr.set_index(['pairs'], inplace = True)
+
+    #d rop level columns
+    mult_corr.drop(columns=['level_1', 'level_0'], inplace = True)
+
+    # rename correlation column as cc rather than 0
+    mult_corr.columns = ['cc']
+
+    # drop duplicates. This could be dangerous if you have variables perfectly correlated with variables other than themselves.
+    # for the sake of exercise, kept it in.
+    mult_corr.drop_duplicates(inplace=True)
+
+    highly_corr = mult_corr[(mult_corr.cc>threshold)] #& (test.cc <1)]
+
+    return highly_corr
